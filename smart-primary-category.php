b3c3ba87b67b8fb3d6653b1e34b51b46cce6dadb
Oceas <scott@getcodesmart.com>
2019-03-17 15:42:40 -0400

    Updated plugin register file

---
+++ smart-primary-category.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Plugin Name:  Smart Primary Category
+Plugin URI:   https://scottkeithanderson.com/smart-primary-category
+Description:  Easily tag your WordPress content with a parent category and fetch it.
+Version:      0.1
+Author:       Scott Anderson
+Author URI:   https://scottkeithanderson.com
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  ska
+Domain Path:  /languages
+*/
+
+
+
+?>
